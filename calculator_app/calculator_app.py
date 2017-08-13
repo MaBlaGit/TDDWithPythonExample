e395b1a8d9f1a3f7afd10431b0d31f0bbdab6a0c
Mariusz Blajet <mariuszblajet@gmail.com>
2017-08-13 15:51:52 +0200

    app static int verification added

--- calculator_app/calculator_app.py
+++ calculator_app/calculator_app.py
@@ -5,8 +5,16 @@
 class Calculator(object):
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = Calculator.check_if_int_or_float(x)
+        self.y = Calculator.check_if_int_or_float(y)
+
+    @staticmethod
+    def check_if_int_or_float(number):
+        if not isinstance(number, (int, float)):
+            raise Exception("Arguments passed to object should be of <class 'int'> or "
+                            "<class 'float'>, not {0}".format(type(number)))
+        else:
+            return number
 
     def add(self):
         return self.x + self.y
@@ -16,3 +24,6 @@ class Calculator(object):
 
     def multiply(self):
         return self.x * self.y
+
+    def divide(self):
+        return self.x / self.y
